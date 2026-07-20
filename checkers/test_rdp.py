c59702a2b2a78226ed3d2bd4629a2ba278aa742c
G33l0 <121323412+G33l0@users.noreply.github.com>
2026-07-20 07:36:35 -0500

    Delete test_rdp.py

--- checkers/test_rdp.py
+++
@@ -1,8 +0,0 @@
-from checkers.rdparena import check
-
-email = "issamsoboh96@gmail.com"
-password = "Msi249336*"
-
-success, msg = check(email, password)
-print(f"Success: {success}")
-print(f"Message: {msg}")
