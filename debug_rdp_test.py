5a05244adc584c3317fd9d4bd25a34815e0c1418
G33l0 <121323412+G33l0@users.noreply.github.com>
2026-07-21 05:37:32 -0500

    Update debug_rdp_test.py

--- debug_rdp_test.py
+++ debug_rdp_test.py
@@ -1,25 +1,5 @@
-import requests
-
+from checkers.rdparena import check
 email = "issamsoboh96@gmail.com"
 password = "Msi249336*"
-
-session = requests.Session()
-session.headers.update({
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Origin": "https://www.rdparena.com/",
-    "Referer": "https://www.rdparena.com/"
-})
-
-response = session.post(
-    "https://www.rdparena.com/payments/login",
-    data={"email": email, "password": password},
-    allow_redirects=False
-)
-
-print("Status:", response.status_code)
-print("Headers:", response.headers)
-print("Cookies:", session.cookies.get_dict())
-print("\n--- Full HTML Response ---\n")
-print(response.text)   # Print everything
+success, msg = check(email, password)
+print(success, msg)
